56fde0078e1d1c9f16011229891658d041023ef3
fcatic031 <fcatic031@gmail.com>
2023-09-26 23:57:33 +0200

    JSON (GSON) pokusaj

--- netbeans/KucniBudget/src/main/java/zavrsni/Start.java
+++ netbeans/KucniBudget/src/main/java/zavrsni/Start.java
@@ -4,6 +4,8 @@
 
 package zavrsni;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import zavrsni.controller.ObradaDnevnaPotrosnja;
 import zavrsni.controller.ObradaObitelj;
 import zavrsni.model.DnevnaPotrosnja;
@@ -17,6 +19,7 @@ import zavrsni.view.Autorizacija;
 import zavrsni.view.SplashScreen;
 
 import javax.swing.*;
+import java.util.List;
 
 /**
  *
@@ -26,15 +29,16 @@ public class Start {
 
     public static void main(String[] args) {
         //HibernateUtil.getSession();
-        new FakerInsert();
+        //new FakerInsert();
         //dodavanjeObitelji();
 
         //JPanel panel = new SplashScreen().panel;
-        //Alati.runApp(panel);
-
+        //Alati.runApp(panel,"LOADING...");
+        JSON();
     }
 
     private static void dodavanjeObitelji(){
+
         ObradaObitelj ob = new ObradaObitelj();
 
         Obitelj o = new Obitelj();
@@ -46,6 +50,22 @@ public class Start {
             System.out.println(b.getPoruka());
         }
         System.out.println("GOTOVOOO");
+    }
+
+    private static void JSON(){
+        FakerInsert faker = new FakerInsert();
+        //Gson gson = new Gson();
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        //Obitelj obitelj = gson.fromJson();
+        String json;
+        for (Obitelj o : faker.obitelji){
+
+            json = gson.toJson(o);
+            System.out.println(o.getObiteljskoPrezime());
+        }
+        //System.out.println(json);
     }
 
 }
