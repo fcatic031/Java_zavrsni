a5a223e65a47ca9f0a574df5915e445cb8beb59f
fcatic031 <fcatic031@gmail.com>
2023-10-09 14:10:26 +0200

    RadioButton spol

--- netbeans/KucniBudget/src/main/java/zavrsni/view/ProzorKorisnik.java
+++ netbeans/KucniBudget/src/main/java/zavrsni/view/ProzorKorisnik.java
@@ -3,16 +3,17 @@ package zavrsni.view;
 import com.github.lgooddatepicker.components.DatePicker;
 import com.github.lgooddatepicker.components.DatePickerSettings;
 import zavrsni.controller.ObradaKorisnik;
+import zavrsni.controller.ObradaObitelj;
 import zavrsni.model.Korisnik;
+import zavrsni.model.Obitelj;
 import zavrsni.util.Alati;
+import zavrsni.util.BudgetException;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -25,8 +26,6 @@ public class ProzorKorisnik implements ViewInterface{
     private JTextField txtPrezime;
     private JTextField txtEmail;
     private JTextField txtDatumRodjenja;
-    private JTextField txtObitelj;
-    private JTextField txtUloga;
     private JTextField txtLozinka;
     private JButton btnNazad;
     private JButton btnDodaj;
@@ -36,11 +35,17 @@ public class ProzorKorisnik implements ViewInterface{
     private JLabel lblTrazi;
     private JButton btnTrazi;
     private DatePicker dpDatum;
+    private JComboBox cmbObitelj;
+    private JRadioButton rbtnZensko;
+    private JRadioButton rbtnMusko;
+    private JLabel lblSpolError;
     private ObradaKorisnik obrada;
 
 public ProzorKorisnik() {
     obrada = new ObradaKorisnik();
+    loadObitelj();
     settingsDate();
+    settingsSpol();
 
     load();
     lstValues.addListSelectionListener(new ListSelectionListener() {
@@ -94,9 +99,21 @@ public ProzorKorisnik() {
         }
     });
 
+    rbtnMusko.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            settingsSpol();
+        }
+    });
+    rbtnZensko.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            settingsSpol();
+        }
+    });
 }
 
-    public void settingsDate(){
+    private void settingsDate(){
         DatePickerSettings dps = new DatePickerSettings(Locale.of("hr","HR"));
         dps.setFormatForDatesCommonEra("dd. MM. YYYY.");
         dps.setTranslationClear("Očisti");
@@ -105,6 +122,31 @@ public ProzorKorisnik() {
 
 
     }
+
+    private void loadObitelj(){
+        DefaultComboBoxModel<Obitelj> model = new DefaultComboBoxModel<>();
+        Obitelj o = new Obitelj();
+        o.setId(0);
+        o.setObiteljskoPrezime("Odaberite kategoriju: ");
+        model.addElement(o);
+
+        model.addAll(new ObradaObitelj().read());
+
+        cmbObitelj.setModel(model);
+        cmbObitelj.repaint();
+    }
+
+    private void settingsSpol(){
+        if (rbtnMusko.isSelected()){
+            rbtnZensko.setSelected(false);
+            rbtnMusko.setSelected(true);
+        }
+        if (rbtnZensko.isSelected()){
+            rbtnMusko.setSelected(false);
+            rbtnZensko.setSelected(true);
+        }
+    }
+
 
     @Override
     public void load() {
@@ -121,11 +163,19 @@ public ProzorKorisnik() {
         e.setIme(txtIme.getText());
         e.setPrezime(txtPrezime.getText());
         e.setEmail(txtEmail.getText());
+        e.setObitelj((Obitelj) cmbObitelj.getSelectedItem());
+        if (rbtnMusko.isSelected()){
+            e.setSpol(true);
+        } else if (rbtnZensko.isSelected()){
+            e.setSpol(false);
+        } else{
+            lblSpolError.setText("Spol nije označen");
+        }
+
         LocalDate ld = dpDatum.getDate();
 
         e.setDatumRodjenja(Date.from(ld.atStartOfDay(ZoneId.systemDefault()).toInstant()));
 
-        //e.setObitelj();
     }
 
     @Override
@@ -135,7 +185,16 @@ public ProzorKorisnik() {
         txtIme.setText(e.getIme());
         txtPrezime.setText(e.getPrezime());
         txtEmail.setText(e.getEmail());
-        txtObitelj.setText(e.getObitelj().getObiteljskoPrezime());
+        cmbObitelj.setSelectedItem(e.getObitelj());
+
+        if (e.isSpol()){
+            rbtnMusko.setSelected(true);
+            rbtnZensko.setSelected(false);
+        } else {
+            rbtnZensko.setSelected(true);
+            rbtnMusko.setSelected(false);
+        }
+
         if(e.getDatumRodjenja()==null){
             dpDatum.setDate(null);
         }else{
