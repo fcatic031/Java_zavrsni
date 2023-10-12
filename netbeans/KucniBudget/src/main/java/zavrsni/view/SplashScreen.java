82eeff69fcc2ca1b764dd36617a23216db3e4b02
fcatic031 <fcatic031@gmail.com>
2023-10-12 19:31:24 +0200

    ProgressBar dodan
    Jos trebam iskušavati malo

--- netbeans/KucniBudget/src/main/java/zavrsni/view/SplashScreen.java
+++ netbeans/KucniBudget/src/main/java/zavrsni/view/SplashScreen.java
@@ -2,12 +2,14 @@ package zavrsni.view;
 
 import org.hibernate.Session;
 import zavrsni.util.Alati;
+import zavrsni.util.BudgetException;
 import zavrsni.util.HibernateUtil;
 
 import javax.swing.*;
 
 public class SplashScreen {
     public JPanel panel;
+    private JProgressBar progressBar;
 
     public SplashScreen(){
         load();
@@ -25,14 +27,28 @@ public class SplashScreen {
                 System.out.println("Problem s bazom");
                 return;
             }
-
+            loading();
             JPanel panel1 = new Autorizacija().panel;
             JFrame frame = Alati.getFrame();
             Alati.runApp(panel1,"Autorizacija");
             Alati.disposeApp(frame);
 
-
         }
 
+        public void loading(){
+            progressBar.setMinimum(0);
+            progressBar.setMaximum(100);
+            progressBar.setValue(0);
+
+            for (int i = 0; i<100; i++){
+                progressBar.setValue(i);
+
+                try{
+                    sleep(100);
+                } catch (InterruptedException ex){
+
+                }
+            }
+        }
     }
 }
