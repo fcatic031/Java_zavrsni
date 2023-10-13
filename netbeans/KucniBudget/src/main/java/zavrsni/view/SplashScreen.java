236bf3a69978b4bba00df513081b67a395504316
fcatic031 <fcatic031@gmail.com>
2023-10-13 19:08:56 +0200

    Promjena slike i sređen progressBar

--- netbeans/KucniBudget/src/main/java/zavrsni/view/SplashScreen.java
+++ netbeans/KucniBudget/src/main/java/zavrsni/view/SplashScreen.java
@@ -6,13 +6,17 @@ import zavrsni.util.BudgetException;
 import zavrsni.util.HibernateUtil;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SplashScreen {
     public JPanel panel;
     private JProgressBar progressBar;
+    private JLabel lblSlika;
 
     public SplashScreen(){
+
         load();
+        //new Loading().loading();
     }
     private void load(){
         new Loading().start();
@@ -22,12 +26,14 @@ public class SplashScreen {
 
         @Override
         public void run(){
+            lblSlika.setIcon(new ImageIcon(new ImageIcon("src\\main\\resources\\HomeBudgetSlika.jpeg").getImage().getScaledInstance(500,500,Image.SCALE_DEFAULT)));
+            loadingBar(progressBar,0,50);
             Session s = HibernateUtil.getSession();
+            loadingBar(progressBar,50,100);
             if (s.getMetamodel().getEntities().isEmpty()){
                 System.out.println("Problem s bazom");
                 return;
             }
-            loading();
             JPanel panel1 = new Autorizacija().panel;
             JFrame frame = Alati.getFrame();
             Alati.runApp(panel1,"Autorizacija");
@@ -35,16 +41,16 @@ public class SplashScreen {
 
         }
 
-        public void loading(){
+        public void loadingBar(JProgressBar progressBar,int min,int max){
             progressBar.setMinimum(0);
             progressBar.setMaximum(100);
             progressBar.setValue(0);
 
-            for (int i = 0; i<100; i++){
+            for (int i = min; i<=max; i++){
                 progressBar.setValue(i);
 
                 try{
-                    sleep(100);
+                    sleep(35);
                 } catch (InterruptedException ex){
 
                 }
