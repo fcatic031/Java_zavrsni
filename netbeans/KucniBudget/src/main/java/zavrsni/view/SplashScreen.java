c68bbf6f900b2cce7ef53c18b584cdbdb9488cea
fcatic031 <fcatic031@gmail.com>
2023-10-20 18:46:07 +0200

    Splash screen - nove karakteristike

--- netbeans/KucniBudget/src/main/java/zavrsni/view/SplashScreen.java
+++ netbeans/KucniBudget/src/main/java/zavrsni/view/SplashScreen.java
@@ -12,11 +12,12 @@ public class SplashScreen {
     public JPanel panel;
     private JProgressBar progressBar;
     private JLabel lblSlika;
+    private JLabel lblSlika2;
+    private Boolean pocetak;
 
-    public SplashScreen(){
-
+    public SplashScreen(Boolean pocetak){
+        this.pocetak = pocetak;
         load();
-        //new Loading().loading();
     }
     private void load(){
         new Loading().start();
@@ -26,25 +27,51 @@ public class SplashScreen {
 
         @Override
         public void run(){
-            lblSlika.setIcon(new ImageIcon(new ImageIcon("src\\main\\resources\\HomeBudgetSlika.jpeg").getImage().getScaledInstance(500,500,Image.SCALE_DEFAULT)));
-            loadingBar(progressBar,0,50);
-            Session s = HibernateUtil.getSession();
-            loadingBar(progressBar,50,100);
-            if (s.getMetamodel().getEntities().isEmpty()){
-                System.out.println("Problem s bazom");
-                return;
-            }
-            JPanel panel1 = new Autorizacija().panel;
-            JFrame frame = Alati.getFrame();
-            Alati.runApp(panel1,"Autorizacija");
-            Alati.disposeApp(frame);
 
+            if (pocetak) {
+                lblSlika.setIcon(new ImageIcon(new ImageIcon("src\\main\\resources\\HomeBudgetSlika.jpeg").getImage().getScaledInstance(500, 500, Image.SCALE_DEFAULT)));
+                loadingBar(progressBar, 0, 50);
+                Session s = HibernateUtil.getSession();
+                loadingBar(progressBar, 50, 100);
+                if (s.getMetamodel().getEntities().isEmpty()) {
+                    System.out.println("Problem s bazom");
+                    return;
+                }
+                JPanel panel1 = new Autorizacija().panel;
+                JFrame frame = Alati.getFrame();
+                Alati.runApp(panel1, "Autorizacija", true);
+                Alati.disposeApp(frame);
+                lblSlika.setIcon(null);
+            } else {
+                //lblSlika.setIcon(new ImageIcon(new ImageIcon("src\\main\\resources\\HomeBudgetSlika.jpeg").getImage().getScaledInstance(500, 500, Image.SCALE_DEFAULT)));
+
+                JFrame frame = Alati.getFrame();
+                Alati.panelIzbornik = new Izbornik().panel;
+                loadingBar(progressBar, 0, 20);
+
+                Alati.panelKorisnik = new ProzorKorisnik().panel;
+                loadingBar(progressBar, 20, 40);
+
+                Alati.panelObitelj = new ProzorObitelj().panel;
+                loadingBar(progressBar, 40, 60);
+
+                Alati.panelKategorija = new ProzorKategorija().panel;
+                loadingBar(progressBar, 60, 80);
+
+                Alati.panelDnevnaPotrosnja = new ProzorDnevnaPotrosnja().panel;
+                loadingBar(progressBar, 80, 100);
+
+                Alati.disposeApp(frame);
+                Alati.runApp(Alati.panelIzbornik,"Izbornik",true);
+
+            }
         }
 
         public void loadingBar(JProgressBar progressBar,int min,int max){
             progressBar.setMinimum(0);
             progressBar.setMaximum(100);
-            progressBar.setValue(0);
+            progressBar.setValue(min);
+            progressBar.setString("Očitavanje");
 
             for (int i = min; i<=max; i++){
                 progressBar.setValue(i);
